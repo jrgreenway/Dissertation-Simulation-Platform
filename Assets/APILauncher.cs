dcaf9b614d1e28e636ad82788c0c6f47bf62f239
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-09-04 15:50:03 +0100

    removed api from this

--- Assets/APILauncher.cs
+++ Assets/APILauncher.cs
@@ -1,23 +1,20 @@
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
 
+
 public class ApiRunner
 { 
-    private static Process process;
     public static IEnumerator Run(string args)
     {
         string url = "http://localhost:8000/model";
 
-        // Create a UnityWebRequest for a POST request
         UnityWebRequest request = new UnityWebRequest(url, "POST");
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(args);
         request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
 
-        // Send the request and wait for a response
         yield return request.SendWebRequest();
 
         if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
